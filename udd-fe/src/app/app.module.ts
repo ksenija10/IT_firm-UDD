7b3d1bac3d11fae6e9b2f0819ff4861a7c45ff28
Ksenija Prcic <ksenija.prcic1998@gmail.com>
2022-02-03 10:27:53 +0100

    Initial search forms on FE created
    Made simple search form smaller a bit
    Created initial version of Advanced search form
    Created initial version of GeoLocation search form

--- udd-fe/src/app/app.module.ts
+++ udd-fe/src/app/app.module.ts
@@ -8,7 +8,10 @@ import { SearchComponent } from './components/search/search.component';
 
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GeoSearchFormComponent } from './components/geo-search-form/geo-search-form.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +19,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SimpleSearchFormComponent,
     AdvancedSearchFormComponent,
     SearchComponent,
+    GeoSearchFormComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatTabsModule,
     MatSelectModule,
+    MatCheckboxModule,
+    MatButtonToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ udd-fe/src/app/components/geo-search-form/geo-search-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-geo-search-form',
+  templateUrl: './geo-search-form.component.html',
+  styleUrls: ['./geo-search-form.component.css']
+})
+export class GeoSearchFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
